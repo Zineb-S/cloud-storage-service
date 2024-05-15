5e54f539352c673af7ec17139fe690abdb72fc95
Zineb-S <zineb.selmouni@ynov.com>
2024-05-15 11:23:41 +0000

    files display

--- client/src/components/user/Profile.js
+++ client/src/components/user/Profile.js
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../NavBar';
 import { useAuth } from '../../services/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './Profile.css'; // Import the CSS file
+import './Profile.css';
+import { FaFileImage, FaFileVideo, FaFileAlt, FaDownload, FaTrash, FaShareAlt } from 'react-icons/fa';
 
 const Profile = () => {
-    const { logout, user } = useAuth(); // Assume `user` contains the email
+    const { logout, user } = useAuth();
     const navigate = useNavigate();
     const [file, setFile] = useState(null);
     const [folder, setFolder] = useState('');
+    const [files, setFiles] = useState([]);
+
+    useEffect(() => {
+        const fetchFiles = async () => {
+            try {
+                console.log('Fetching files for user:', user.email);
+                const response = await axios.get('https://0a215c9039ba4770a11d847aa1b501ce.vfs.cloud9.us-east-1.amazonaws.com:8080/files', {
+                    params: { username: user.email },
+                    withCredentials: true
+                });
+                console.log('Files fetched successfully:', response.data);
+                setFiles(response.data);
+            } catch (error) {
+                console.error('Error fetching files:', error);
+            }
+        };
+
+        fetchFiles();
+    }, [user.email]);
 
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
@@ -28,17 +48,21 @@ const Profile = () => {
         const formData = new FormData();
         formData.append('file', file);
         formData.append('folder', folder);
-        formData.append('username', user.email); // Add the email as the username to the form data
-        console.log('username', user.email);
+        formData.append('username', user.email);
+
         try {
+            console.log('Uploading file:', file);
             const response = await axios.post('https://0a215c9039ba4770a11d847aa1b501ce.vfs.cloud9.us-east-1.amazonaws.com:8080/upload', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 },
                 withCredentials: true
             });
+            console.log('File uploaded successfully:', response.data);
             alert('File uploaded successfully: ' + response.data.location);
+            setFiles(prevFiles => [...prevFiles, response.data]); // Update file list after upload
         } catch (error) {
+            console.error('Error uploading file:', error);
             alert('Error uploading file: ' + error.message);
         }
     };
@@ -46,6 +70,111 @@ const Profile = () => {
     const handleLogout = () => {
         logout();
         navigate('/login');
+    };
+
+    const handleFileDelete = async (fileID) => {
+        try {
+            const response = await axios.delete('https://0a215c9039ba4770a11d847aa1b501ce.vfs.cloud9.us-east-1.amazonaws.com:8080/delete', {
+                params: { fileID },
+                withCredentials: true
+            });
+            alert('File deleted successfully');
+            setFiles(files.filter(file => file.fileID.N !== fileID));
+        } catch (error) {
+            console.error('Error deleting file:', error);
+            alert('Error deleting file: ' + error.message);
+        }
+    };
+
+    const handleFileShare = async (fileID) => {
+        try {
+            const response = await axios.get('https://0a215c9039ba4770a11d847aa1b501ce.vfs.cloud9.us-east-1.amazonaws.com:8080/share', {
+                params: { fileID },
+                withCredentials: true
+            });
+            alert('Share link: ' + response.data.shareLink);
+        } catch (error) {
+            console.error('Error generating share link:', error);
+            alert('Error generating share link: ' + error.message);
+        }
+    };
+
+    const getFileTypeIcon = (fileType) => {
+        switch (fileType) {
+            case 'image/png':
+            case 'image/jpeg':
+                return <FaFileImage />;
+            case 'video/mp4':
+                return <FaFileVideo />;
+            default:
+                return <FaFileAlt />;
+        }
+    };
+
+    const renderFiles = (files) => {
+        const folders = {};
+        const userFiles = [];
+
+        files.forEach(file => {
+            const folder = file.FilePath.S.split('/')[0];
+            if (folder === user.email) {
+                userFiles.push(file);
+            } else {
+                if (!folders[folder]) {
+                    folders[folder] = [];
+                }
+                folders[folder].push(file);
+            }
+        });
+
+        return (
+            <div>
+                <h3>Files:</h3>
+                <ul>
+                    {userFiles.map((file, index) => (
+                        <li key={index} className="file-item">
+                            <span className="file-icon">{getFileTypeIcon(file.FileType.S)}</span>
+                            <span className="file-name">{file.FilePath.S.split('/').pop()}</span>
+                            <span className="file-size">({(file.FileSize.N / 1024).toFixed(2)} KB)</span>
+                            <button onClick={() => window.open(file.Location.S, '_blank')}><FaDownload /></button>
+                            <button onClick={() => handleFileDelete(file.fileID.N)}><FaTrash /></button>
+                            <button onClick={() => handleFileShare(file.fileID.N)}><FaShareAlt /></button>
+                        </li>
+                    ))}
+                </ul>
+                <h3>Folders:</h3>
+                {Object.keys(folders).map((folder, index) => (
+                    <div key={index}>
+                        <button type="button" onClick={() => toggleAccordion(index)}>
+                            {folder}
+                        </button>
+                        <div id={`accordion-${index}`} style={{ display: 'none' }}>
+                            <ul>
+                                {folders[folder].map((file, idx) => (
+                                    <li key={idx} className="file-item">
+                                        <span className="file-icon">{getFileTypeIcon(file.FileType.S)}</span>
+                                        <span className="file-name">{file.FilePath.S.split('/').pop()}</span>
+                                        <span className="file-size">({(file.FileSize.N / 1024).toFixed(2)} KB)</span>
+                                        <button onClick={() => window.open(file.Location.S, '_blank')}><FaDownload /></button>
+                                        <button onClick={() => handleFileDelete(file.fileID.N)}><FaTrash /></button>
+                                        <button onClick={() => handleFileShare(file.fileID.N)}><FaShareAlt /></button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    const toggleAccordion = (index) => {
+        const element = document.getElementById(`accordion-${index}`);
+        if (element.style.display === 'none') {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none';
+        }
     };
 
     return (
@@ -69,6 +198,7 @@ const Profile = () => {
                     <button type="submit" className="upload-button">Upload File</button>
                 </form>
                 <button onClick={handleLogout} className="logout-button">Logout</button>
+                {renderFiles(files)}
             </div>
         </div>
     );
--- server/server.js
+++ server/server.js
@@ -2,18 +2,32 @@ const express = require('express');
 const cors = require('cors');
 const formidable = require('formidable');
 const { S3 } = require('@aws-sdk/client-s3');
-const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBClient, PutItemCommand, QueryCommand, GetItemCommand, DeleteItemCommand } = require('@aws-sdk/client-dynamodb');
 const { Upload } = require('@aws-sdk/lib-storage');
+const { GetObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 const fs = require('fs');
 const path = require('path');
-const app = express();
 
-app.use(cors({
-    origin: 'https://0a215c9039ba4770a11d847aa1b501ce.vfs.cloud9.us-east-1.amazonaws.com:8081',
+const app = express();
+const port = 8080;
+
+const allowedOrigins = ['https://0a215c9039ba4770a11d847aa1b501ce.vfs.cloud9.us-east-1.amazonaws.com:8081'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-}));
+};
+
+app.use(cors(corsOptions));
 
 const s3 = new S3({ region: 'us-east-1' });
 const dynamoDbClient = new DynamoDBClient({ region: 'us-east-1' });
@@ -23,34 +37,58 @@ const saveFileMetadata = async (username, key, location, fileSize, fileType) => 
     const params = {
         TableName: 'bexcloud',
         Item: {
-            fileID: { N: fileID.toString() },  // Ensure fileID is treated as a number
+            fileID: { N: fileID.toString() },
             username: { S: username },
             FilePath: { S: key },
             Location: { S: location },
-            FileSize: { N: fileSize.toString() },  // Ensure FileSize is treated as a number
+            FileSize: { N: fileSize.toString() },
             FileType: { S: fileType },
             CreatedAt: { S: new Date().toISOString() }
         }
     };
     try {
+        console.log('Saving file metadata:', params);
         await dynamoDbClient.send(new PutItemCommand(params));
+        console.log('File metadata saved successfully');
     } catch (error) {
         console.error('Failed to save file metadata:', error);
         throw error;
     }
 };
 
+app.get('/files', async (req, res) => {
+    const { username } = req.query;
+    console.log('Fetching files for user:', username);
+
+    const params = {
+        TableName: 'bexcloud',
+        IndexName: 'UsernameIndex',
+        KeyConditionExpression: 'username = :username',
+        ExpressionAttributeValues: {
+            ':username': { S: username }
+        }
+    };
+
+    try {
+        const data = await dynamoDbClient.send(new QueryCommand(params));
+        console.log('Files fetched successfully:', data.Items);
+        res.status(200).json(data.Items);
+    } catch (error) {
+        console.error('Failed to fetch user files:', error);
+        res.status(500).send('Failed to fetch user files.');
+    }
+});
+
 app.post('/upload', (req, res) => {
     const form = new formidable.IncomingForm();
 
     form.parse(req, async (err, fields, files) => {
         if (err) {
+            console.error('Error parsing form data:', err);
             return res.status(500).json({ error: 'Error parsing form data' });
         }
 
         const username = Array.isArray(fields.username) ? fields.username[0] : fields.username || 'current user';
-        
-        // Ensure folder is a string and not empty
         let folder = Array.isArray(fields.folder) ? fields.folder[0] : fields.folder;
         if (!folder || folder.trim() === '') {
             folder = username; // Use username or a default value
@@ -63,6 +101,7 @@ app.post('/upload', (req, res) => {
         console.log('Parsed file:', file);
 
         if (!file) {
+            console.error('No file provided');
             return res.status(400).json({ error: 'File is not provided' });
         }
 
@@ -90,11 +129,12 @@ app.post('/upload', (req, res) => {
             });
 
             parallelUploads3.on('httpUploadProgress', (progress) => {
-                console.log(progress);
+                console.log('Upload progress:', progress);
             });
 
             const uploadResult = await parallelUploads3.done();
 
+            console.log('File uploaded successfully:', uploadResult);
             await saveFileMetadata(username, uploadResult.Key, uploadResult.Location, file.size.toString(), file.mimetype);
 
             res.status(201).send({
@@ -108,7 +148,79 @@ app.post('/upload', (req, res) => {
     });
 });
 
-const port = 8080;
+app.delete('/delete', async (req, res) => {
+    const { fileID } = req.query;
+
+    const getParams = {
+        TableName: 'bexcloud',
+        Key: {
+            fileID: { N: fileID }
+        }
+    };
+
+    try {
+        const data = await dynamoDbClient.send(new GetItemCommand(getParams));
+        const file = data.Item;
+
+        if (!file) {
+            return res.status(404).send('File not found');
+        }
+
+        const deleteParams = {
+            Bucket: 'bexcloud',
+            Key: file.FilePath.S
+        };
+
+        await s3.send(new DeleteObjectCommand(deleteParams));
+
+        const deleteDbParams = {
+            TableName: 'bexcloud',
+            Key: {
+                fileID: { N: fileID }
+            }
+        };
+
+        await dynamoDbClient.send(new DeleteItemCommand(deleteDbParams));
+
+        res.status(200).send('File deleted successfully');
+    } catch (error) {
+        console.error('Failed to delete file:', error);
+        res.status(500).send('Failed to delete file.');
+    }
+});
+
+app.get('/share', async (req, res) => {
+    const { fileID } = req.query;
+
+    const getParams = {
+        TableName: 'bexcloud',
+        Key: {
+            fileID: { N: fileID }
+        }
+    };
+
+    try {
+        const data = await dynamoDbClient.send(new GetItemCommand(getParams));
+        const file = data.Item;
+
+        if (!file) {
+            return res.status(404).send('File not found');
+        }
+
+        const getObjectParams = {
+            Bucket: 'bexcloud',
+            Key: file.FilePath.S
+        };
+
+        const shareLink = await getSignedUrl(s3, new GetObjectCommand(getObjectParams), { expiresIn: 3600 });
+
+        res.status(200).send({ shareLink });
+    } catch (error) {
+        console.error('Failed to generate share link:', error);
+        res.status(500).send('Failed to generate share link.');
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
